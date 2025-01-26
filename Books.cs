7a111e6486694f183b52887c4cb3ced201143399
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-01-26 23:23:36 +0200

    Add files via upload

---
+++ Books.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_1
+{
+    /// <summary>
+    /// класс Книги
+    /// </summary>
+    internal class Books
+    {
+        /// <summary>
+        /// Название книги
+        /// </summary>
+        public string name = "Бородино";
+
+        /// <summary>
+        /// Автор
+        /// </summary>
+        public string author = "Лермонтов";
+
+        /// <summary>
+        /// Год выпуска
+        /// </summary>
+        public int year = 2005;
+
+        /// <summary>
+        /// Объём в листах
+        /// </summary>
+        public int count = 120;
+
+        /// <summary>
+        /// Метод вывода информации о книге.
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"Название книги: {name}\nАвтор книги: " +
+                $"{author}\nГод выпуска: {year}\nОбъём в листах: {count}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-й вариант
+            Books books = new Books();
+            books.Print();
+            Console.WriteLine();
+
+            //2-й вариант
+            string nameBooks = books.name;
+            string authorBooks = books.author;
+            int yearBooks = books.year;
+            int countBooks = books.count;
+            Console.WriteLine($"Название книги: {nameBooks}\nАвтор книги: " +
+                $"{authorBooks}\nГод выпуска: {yearBooks}\nОбъём в листах: {countBooks}");
+            Console.WriteLine();
+
+            //3-й вариант
+            books.name = "Преступление и наказание";
+            books.author = "Достоевский";
+            books.year = 2023;
+            books.count = 400;
+            books.Print();
+            Console.WriteLine();
+
+            //4-й вариант
+            Console.Write("Название книги: ");
+            books.name = Console.ReadLine();
+            Console.Write("Автор книги: ");
+            books.author = Console.ReadLine();
+            Console.Write("Год выпуска: ");
+            books.year = int.Parse(Console.ReadLine());
+            Console.Write("Объём в листах: ");
+            books.count = int.Parse(Console.ReadLine());
+            books.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
